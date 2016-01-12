b1dfd038c4994e2a8676a27ddcdb5c8e5a6e78dc
Tina Ostrander <laptop@itadmins-air-2.tech.div>
2016-01-12 10:43:46 -0800

    index page committed

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Beehive Prototype Home</title>
+  </head>
+  <body>
+    <p>I love Bees.</p>
+    <?php
+      phpinfo();
+    ?>
+  </body>
+</html>    
